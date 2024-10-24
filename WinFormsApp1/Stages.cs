4effccec36a9443e7a064a60d1cd83a280bf7748
Diogo Graça <a28004@alunos.ipca.pt>
2024-10-24 23:34:17 +0100

    Upload-1

---
+++ WinFormsApp1/Stages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertManager
+{
+    public class Stages
+    {
+        private string Location { get; set; }
+        private int Capacity { get; set; }
+
+        public Stages(string location, int capacity)
+        {
+            this.Location = location;
+            this.Capacity = capacity;
+        }
+
+        public string location
+        {
+            get { return this.Location; }
+            set { this.Location = value; }
+        }
+
+        public int capacity
+        {
+            get { return this.Capacity; }
+            set { this.Capacity = value; }
+        }
+    }
+}
